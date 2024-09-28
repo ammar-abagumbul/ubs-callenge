e681fbfceb01cfe73884e39612f3709ca55a5a95
Rehman Riyaz Ur <riyaz@connect.hku.hk>
2024-09-28 21:29:37 +0800

    Update digitalColony.py

--- routes/digitalColony.py
+++ routes/digitalColony.py
@@ -33,4 +33,4 @@ def evaluate():
         for _ in range(generations):
             colony = next_generation(colony)
         result.append(str(cal_weight(colony)))
-        return jsonify(result)
+    return jsonify(result)
