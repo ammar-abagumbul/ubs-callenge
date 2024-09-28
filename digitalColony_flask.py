9d6f4a63130216ccbc3f6dc295ca6a50b4e20c79
ammar-abagumbul <ammar96@connect.hku.hk>
2024-09-28 15:21:50 +0800

    new changes

--- digitalColony_flask.py
+++ digitalColony_flask.py
@@ -1,9 +1,16 @@
 from digitalColony import weight_after_gen
+from wordle import playWordle
 from flask import Flask, request, jsonify
+
+####################
+import nltk
+nltk.download('words')
+from nltk.corpus import words
+possible_guesses = [word for word in words.words() if len(word) == 5 and word[0].islower()]
 
 app = Flask(__name__)
 
-@app.route('/weight_after_gen', methods=['POST'])
+@app.route('/wordle-game', methods=['POST'])
 def weight_after_gen_route():
     data = request.get_json()
     colony = data.get('colony')
@@ -12,6 +19,16 @@ def weight_after_gen_route():
         return jsonify({'error': 'Missing colony or generations'}), 400
     result = weight_after_gen(colony, int(generations))
     return jsonify({'result': result})
+
+
+@app.route('/wordle-game', methods=['POST'])
+def wordle_game():
+    data = request.get_json()
+    guessHistory = data.get("guessHistory")
+    evaluationHistory = data.get("evaluationHistory")
+
+    guess = playWordle(guessHistory, evaluationHistory)
+    return jsonify({'guess': guess})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- wordle.py
+++ wordle.py
@@ -4,49 +4,47 @@ import nltk
 nltk.download('words')
 from nltk.corpus import words
 
-possible_guesses = [word for word in words.words() if len(word) == 5 and word[0].islower()]
+def playWordle(guess_history, evaluation_history):
+    possible_guesses = [word for word in words.words() if len(word) == 5 and word[0].islower()]
+    not_solved_idx = [0, 1, 2, 3, 4]
+    correct_letters = []
 
-user_input = input("Guess: ")
-solved = False
-attempt = 1
+    for i in range(len(guess_history)):
+        user_input = guess_history[i]
+        check_string = evaluation_history[i]
+
+        for i in range(5):
+            if i in not_solved_idx:
+              if check_string[i] == "?":
+                continue
+              elif check_string[i] == "-":
+                if user_input[i] in correct_letters:
+                  possible_guesses = [word for word in possible_guesses if user_input[i] != word[i]]
+                  print(str(len(possible_guesses))+ " -")
+                  print(possible_guesses)
+                else:
+                  possible_guesses = [word for word in possible_guesses if user_input[i] not in word]
+                  print(str(len(possible_guesses))+ " -")
+                  print(possible_guesses)
+              elif check_string[i] == "X":
+                possible_guesses = [word for word in possible_guesses if user_input[i] in word and word[i] != user_input[i]]
+                print(possible_guesses)
+                print(str(len(possible_guesses))+ " X")
+              elif check_string[i] == "O":
+                correct_letters.append(user_input[i])
+                not_solved_idx.remove(i)
+                possible_guesses = [word for word in possible_guesses if word[i] == user_input[i]]
+                print(possible_guesses)
+                print(str(len(possible_guesses)) + " O")
+
+    return possible_guesses[0]
+             
+def wordle():
+    possible_guesses = [word for word in words.words() if len(word) == 5 and word[0].islower()]
+
+    user_input = input("Guess: ")
+    solved = False
+    attempt = 1
 
 
-not_solved_idx = [0, 1, 2, 3, 4]
-
-guess_history = []
-evaluation_history = []
-
-correct_letters = []
-
-while(not solved and attempt <= 6):
-    check_string = input("Check String: ")
-    guess_history.append(user_input)
-    evaluation_history.append(check_string)
-
-    for i in range(5):
-        if i in not_solved_idx:
-          if check_string[i] == "?":
-            continue
-          elif check_string[i] == "-":
-            if user_input[i] in correct_letters:
-              possible_guesses = [word for word in possible_guesses if user_input[i] != word[i]]
-              print(str(len(possible_guesses))+ " -")
-              print(possible_guesses)
-            else:
-              possible_guesses = [word for word in possible_guesses if user_input[i] not in word]
-              print(str(len(possible_guesses))+ " -")
-              print(possible_guesses)
-          elif check_string[i] == "X":
-            possible_guesses = [word for word in possible_guesses if user_input[i] in word and word[i] != user_input[i]]
-            print(possible_guesses)
-            print(str(len(possible_guesses))+ " X")
-          elif check_string[i] == "O":
-            correct_letters.append(user_input[i])
-            not_solved_idx.remove(i)
-            possible_guesses = [word for word in possible_guesses if word[i] == user_input[i]]
-            print(possible_guesses)
-            print(str(len(possible_guesses)) + " O")
-
-    user_input = possible_guesses[0]
-    print("Guess: " + user_input)
-    attempt += 1
+    
